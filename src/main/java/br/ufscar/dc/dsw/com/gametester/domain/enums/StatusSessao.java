998a826e1a211d55015a957499faac9adafd19f8
Luiz Gustavo <91857747+lgstavo@users.noreply.github.com>
2025-07-14 11:24:48 -0300

    Delete src directory

--- src/main/java/br/ufscar/dc/dsw/com/gametester/domain/enums/StatusSessao.java
+++
@@ -1,7 +0,0 @@
-package br.ufscar.dc.dsw.com.gametester.domain.enums;
-
-public enum StatusSessao {
-    CRIADO,
-    EM_EXECUCAO,
-    FINALIZADO
-}
--- src/main/java/br/ufscar/dc/dsw/com/gametester/domain/enums/TipoPerfil.java
+++
@@ -1,7 +0,0 @@
-package br.ufscar.dc.dsw.com.gametester.domain.enums;
-
-public enum TipoPerfil {
-    ROLE_ADMINISTRADOR,
-    ROLE_TESTADOR
-    // Se você tiver outros perfis, adicione-os aqui.
-}
